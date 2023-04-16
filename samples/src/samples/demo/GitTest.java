87fa95e8b22abe5fce278da5b98c1c8f67688d62
arifo <b032110259@student.utem.edu.my>
2023-04-16 16:23:56 +0800

    test

--- samples/src/samples/demo/GitTest.java
+++ samples/src/samples/demo/GitTest.java
@@ -1,7 +1,12 @@
 package samples.demo;
 
 public class GitTest {
-    public static void main(String[] args) {
+    private class Location {
+
+	}
+
+	public static void main(String[] args) {
         System.out.println("Git is fun");
+        System.out.println("Git is cool");
     }
 }
